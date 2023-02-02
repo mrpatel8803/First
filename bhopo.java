17302644761091cf86504945e93e825bc0c73dd4
mrpatel8803 <124238624+mrpatel8803@users.noreply.github.com>
2023-02-02 12:52:59 +0530

    Add files via upload
    new add

---
+++ bhopo.java
@@ -0,0 +1,16 @@
+public class bhopo
+{
+	public static void main(String [] args) 
+	{   int x = 89; 
+        
+        int y = 43;
+        
+        int sum = x + y;
+
+
+		System.out.println("addition is :-"+sum);
+		
+	}
+		
+	
+}
